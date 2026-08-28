17a94f1690b1bce5391779c92a4792345351cdf0
homemmaquina24-spec <homemmaquina24@gmail.com>
2026-08-28 17:59:56 +0200

    Criar cérebro base inicial

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/* =====================================================
+   HOMEM E A MÁQUINA
+   CÉREBRO BASE INICIAL
+===================================================== */
+
+
+console.log("Homem e a Máquina iniciada");
+
+
+
+const voiceButton = document.querySelector(".voice-button");
+
+
+
+if (voiceButton) {
+
+
+    voiceButton.addEventListener("click", function(){
+
+
+        console.log("Modo ouvir ativado");
+
+
+    });
+
+
+                    }
