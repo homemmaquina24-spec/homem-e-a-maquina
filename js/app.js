f606afcb4b396d952d512d94b2bed22ca76c327c
homemmaquina24-spec <homemmaquina24@gmail.com>
2026-08-30 08:58:31 +0200

    Update app.jsCriar JavaScript base da interface

--- js/app.js
+++ js/app.js
@@ -1,27 +1,6 @@
 /* =====================================================
    HOMEM E A MÁQUINA
-   CÉREBRO BASE INICIAL
+   JAVASCRIPT BASE
 ===================================================== */
 
-
 console.log("Homem e a Máquina iniciada");
-
-
-
-const voiceButton = document.querySelector(".voice-button");
-
-
-
-if (voiceButton) {
-
-
-    voiceButton.addEventListener("click", function(){
-
-
-        console.log("Modo ouvir ativado");
-
-
-    });
-
-
-                    }
